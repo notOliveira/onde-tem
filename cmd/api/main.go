e1b92a4311f9498c2612d063b23ea76d1536ffa5
Gustavo Oliveira <gustavooliveira.classroom@gmail.com>
2026-04-16 08:59:00 -0300

    fix: fixing formatting

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -19,23 +19,23 @@ func main() {
 	cfg := config.LoadConfig()
 
 	connPool, err := database.NewConnection(log)
-    if err != nil {
-        log.Errorf("failed to connect db: %v", err)
-        return
-    }
-    defer connPool.Close()
+	if err != nil {
+		log.Errorf("failed to connect db: %v", err)
+		return
+	}
+	defer connPool.Close()
 
 	//queries := sqlc.New(connPool)
 
 	//establishmentRepo := postgres.NewEstablishmentRepository(queries)
 
 	valkeyClient := cache.NewValkeyClient(cfg.ValkeyAddr)
-    _ = valkeyClient
+	_ = valkeyClient
 
 	// http.HandleFunc("/establishments", establishmentHandler.HandleCreate)
 
 	log.Infof("Server running on %s", cfg.ServerPort)
-    if err := http.ListenAndServe(cfg.ServerPort, nil); err != nil {
-        log.Errorf("Server error: %v", err)
-    }
+	if err := http.ListenAndServe(cfg.ServerPort, nil); err != nil {
+		log.Errorf("Server error: %v", err)
+	}
 }
